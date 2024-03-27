00c0e8e3fe6e899c999d289c0765de7bcad3efb3
Prabh ‪‪ <singh20prabh@gmail.com>
2024-03-27 13:21:24 +0530

    file structure

--- src/components/Collection/Collection.tsx
+++ src/components/Collection/Collection.tsx
@@ -1,125 +1,12 @@
-import { useState, useEffect } from "react";
-import CollectionCards from "./CollectionCards";
+import CollectionList from "./CollectionList";
 import HeaderCollection from "./HeaderCollection";
-import Buttons from "./Buttons";
-
-interface CollectionData {
-  id: number;
-  image: string;
-  teamName: string;
-  date: string;
-  day: string;
-  time: string;
-  location: string;
-  collection: string;
-}
-
-const data: CollectionData[] = [
-  {
-    id: 1,
-    image: "/collection/collection1.jpg",
-    teamName: "Las Vegas Aviatars",
-    date: "OCT 15",
-    day: "SUN",
-    time: "4.30 PM",
-    location: "Las Vegas Ballpark, Las Vegas, Nevada",
-    collection: "Take Flight Collection",
-  },
-  {
-    id: 2,
-    image: "/collection/collection2.jpg",
-    teamName: "Sacramento River Cats",
-    date: "OCT 15",
-    day: "SUN",
-    time: "4.30 PM",
-    location: "Sytter Health Park, Sacramento, California",
-    collection: "Orange Collection",
-  },
-  {
-    id: 3,
-    image: "/collection/collection3.jpg",
-    teamName: "Las Vegas Aviatars",
-    date: "OCT 15",
-    day: "SUN",
-    time: "4.30 PM",
-    location: "Las Vegas Ballpark, Las Vegas, Nevada",
-    collection: "Take Flight Collection",
-  },
-  {
-    id: 4,
-    image: "/collection/collection2.jpg",
-    teamName: "Las Vegas Aviatars",
-    date: "OCT 15",
-    day: "SUN",
-    time: "4.30 PM",
-    location: "Las Vegas Ballpark, Las Vegas, Nevada",
-    collection: "Take Flight Collection",
-  },
-  {
-    id: 5,
-    image: "/collection/collection3.jpg",
-    teamName: "Las Vegas Aviatars",
-    date: "OCT 15",
-    day: "SUN",
-    time: "4.30 PM",
-    location: "Las Vegas Ballpark, Las Vegas, Nevada",
-    collection: "Take Flight Collection",
-  },
-  // Add more data objects as needed
-];
 
 const Collection: React.FC = () => {
-  const [startIndex, setStartIndex] = useState(0);
-  const [numCardsPerPage, setNumCardsPerPage] = useState(1);
-
-  useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth < 768) {
-        setNumCardsPerPage(1);
-      } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
-        setNumCardsPerPage(2);
-      } else {
-        setNumCardsPerPage(3);
-      }
-    }
-
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial call to set numCardsPerPage based on initial window width
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const nextSlide = () => {
-    if (startIndex < data.length - numCardsPerPage) {
-      setStartIndex(startIndex + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (startIndex > 0) {
-      setStartIndex(startIndex - 1);
-    }
-  };
-
   return (
     <div className="flex items-center justify-center text-center">
       <div className="relative flex h-[918px] w-[350px] flex-col items-center justify-evenly bg-gradient-light dark:bg-gradient-dark md:w-[1240px]">
         <HeaderCollection />
-        <ul className="flex flex-wrap justify-center gap-8">
-          {data
-            .slice(startIndex, startIndex + numCardsPerPage)
-            .map((collectionCards) => (
-              <CollectionCards
-                collectionCards={collectionCards}
-                key={collectionCards.id}
-              />
-            ))}
-        </ul>
-        <Buttons
-          nextSlide={nextSlide}
-          prevSlide={prevSlide}
-          startIndex={startIndex}
-          data={data}
-        />
+        <CollectionList />
       </div>
     </div>
   );
--- src/components/Details/DetailCards.tsx
+++ src/components/Details/DetailCards.tsx
@@ -1,60 +1,13 @@
-import Advertisement from "./Advertisement";
-import Cards from "./Cards";
 import Header from "./Header";
-
-interface CardData {
-  id: number;
-  image: string;
-  teamName: string;
-  totalEvents: string;
-  sports: string;
-}
-
-const data: CardData[] = [
-  {
-    id: 1,
-    image: "/playeriamges/player1.jpg",
-    teamName: "Sacramento River Cats",
-    totalEvents: "48 Events",
-    sports: "Baseball",
-  },
-  {
-    id: 2,
-    image: "/playeriamges/player2.jpg",
-    teamName: "Las Vegas Aviators",
-    totalEvents: "28 Events",
-    sports: "Baseball",
-  },
-  {
-    id: 3,
-    image: "/playeriamges/player3.jpg",
-    teamName: "New Jersey Devils",
-    totalEvents: "15 Events",
-    sports: "Ice Hockey",
-  },
-  {
-    id: 4,
-    image: "/playeriamges/player4.jpg",
-    teamName: "Las Vegas Aviators",
-    totalEvents: "28 Events",
-    sports: "Baseball",
-  },
-];
+import CardList from "./CardList";
 
 const DetailCards: React.FC = () => {
   return (
     <div className="flex justify-center px-2 py-10">
       <div className="flex flex-col gap-6 xl:h-[673px] xl:w-[1241px]">
         <Header />
-        <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-          {data.map((card) => (
-            <Cards cards={card} key={card.id} />
-          ))}
-          <Advertisement />
-        </ul>
-        <button className="mx-auto mt-6 h-[46px] w-[124px] rounded-[3px] bg-dark-button px-[30px] py-[10px] text-sm font-semibold">
-          See More
-        </button>
+        <CardList />
+       
       </div>
     </div>
   );
