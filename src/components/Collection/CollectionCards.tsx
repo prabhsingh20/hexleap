19ffd75213d383e9c2c337214fe5c35598d0f35f
Prabh ‪‪ <singh20prabh@gmail.com>
2024-04-01 12:24:13 +0530

    fixes

--- src/components/Collection/CollectionCards.tsx
+++ src/components/Collection/CollectionCards.tsx
@@ -17,9 +17,9 @@ const CollectionCards: React.FC<CollectionCardProps> = ({
     collectionCards;
 
   return (
-    <li className="relative flex h-[624px] w-[257px] flex-col items-center gap-3 bg-light-main p-3 shadow-custom dark:bg-dark-secondary">
+    <li className="relative flex h-[624px] w-[257px] flex-col items-center gap-3 bg-light-secondary p-3 shadow-custom dark:bg-dark-secondary">
       <img
-        className="relative h-[401.2px] w-[233.1px] "
+        className="relative h-[401.2px] w-[233.1px] shadow-[0px_8px_16px_0px_#00000033] "
         src={image}
         alt={teamName}
       />
@@ -35,9 +35,15 @@ const CollectionCards: React.FC<CollectionCardProps> = ({
       <button className="mt-2 h-[36px] w-[213px] bg-[#000] text-[12.7px] font-medium">
         {collection}
       </button>
-      <div className="absolute -left-[0.6em]  top-[26.5rem] h-5 w-5 rounded-full  dark:bg-gradient-dark"></div>
-      <div className="absolute -right-[0.6rem]  top-[26.5rem] h-5 w-5 rounded-full  dark:bg-gradient-dark"></div>
-      <div className="absolute   top-[26.6rem] h-2 w-[211px] border-b border-dashed border-[#A9ACB2] dark:border-[#818A97] "></div>
+
+      {/* <div className="absolute -left-[0.6em]  top-[26.5rem] h-5 w-5 rounded-full  dark:bg-gradient-dark"></div> */}
+      {/* <div className="absolute -right-[0.6rem]  top-[26.5rem] h-5 w-5 rounded-full  dark:bg-gradient-dark"></div> */}
+
+      <div className="absolute left-[2px]  top-[26.5rem] h-[20px] w-[20px] -translate-x-[12px] rounded-full bg-gradient-to-b from-[#F9F8FF] to-[#F3F9FF] shadow-[inset_-7px_0px_8px_0px_#00000015] dark:from-[#18282A] dark:to-[#221A2C]"></div>
+
+      <div className="absolute right-[2px]  top-[26.5rem] h-[20px] w-[20px] translate-x-[12px] rounded-full bg-gradient-to-b from-[#F9F8FF] to-[#F3F9FF] shadow-[inset_7px_0px_8px_0px_#00000015] dark:from-[#18282A] dark:to-[#221A2C]"></div>
+
+      <div className=" absolute  left-5 top-[25.8rem] mt-5  w-[84%] border-t-2 border-dashed border-[#A9ACB2]  dark:border-[#818A97]"></div>
     </li>
   );
 };
